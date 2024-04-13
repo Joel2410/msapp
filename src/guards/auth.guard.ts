6c2e67d9faa90bac023a8df07a3d6766196f44d9
Julian Joel Hernandez <jjoel2410@hotmail.com>
2024-04-13 16:55:03 -0400

    CHG Se agregan validaciones al guard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -4,18 +4,22 @@ import {
   Injectable,
   UnauthorizedException,
   Logger,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { DEFAULT_TENANT, IS_PUBLIC_KEY } from '../helpers';
-import { JWT_SECRET } from 'src/config';
+import { DEFAULT_TENANT, IS_PUBLIC_KEY } from '@helpers';
+import { JWT_SECRET } from '../config';
+import { TenantService } from '../modules/tenant/tenant.service';
+import { AccessTokenContent } from 'src/interfaces';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private reflector: Reflector,
+    private tenantService: TenantService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -30,35 +34,47 @@ export class AuthGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    
+
     if (!token) {
       throw new UnauthorizedException();
     }
 
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: JWT_SECRET,
-      });
+      const payload = await this.jwtService.verifyAsync<AccessTokenContent>(
+        token,
+        {
+          secret: JWT_SECRET,
+        },
+      );
 
       //TODO: Crear guard para validar el tenant similar a IS PUBLIC
-      
-      //TODO: Validar que el tenant pertenezca al usuario
+      const tenantId = request?.headers?.host.split('.')[0] || DEFAULT_TENANT;
 
-      const tenantId = request?.headers?.host.split('.')[0];
+      // Validar que el tenant exista y pertenezca al usuario
+      if (!this.tenantService.isUserTenantValid(tenantId, payload.userId)) {
+        const error = `Error: User: ${payload.userId} does not have the tenant: ${tenantId}`;
+        Logger.error(error);
+        throw new UnauthorizedException(error);
+      }
 
+      // Si el token no tiene tenant asignar el tenant por defecto
       if (!payload.tenantId) payload.tenantId = DEFAULT_TENANT;
 
+      // Validar que el tenant del token y el tenant del request sea el mismo
       if (tenantId != payload.tenantId) {
-        Logger.error(
-          `Error: host tenant: ${tenantId}; user tenant: ${payload.tenantId}`,
-        );
-        throw new UnauthorizedException();
+        const error = `Error: host tenant: ${tenantId}; user tenant: ${payload.tenantId}`;
+        Logger.error(error);
+        throw new UnauthorizedException(error);
       }
 
       request['user'] = payload;
       request['tenantId'] = tenantId;
-    } catch {
-      throw new UnauthorizedException();
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException();
     }
 
     return true;
