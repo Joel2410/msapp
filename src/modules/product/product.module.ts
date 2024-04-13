e699690c618ba4a3e66d6e01a6644ad86f08f27a
Julian Joel Hernandez <jjoel2410@hotmail.com>
2024-04-13 16:16:22 -0400

    FIX import tenant module in product module

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { TenantModule } from '../tenant/tenant.module';
 
 @Module({
+  imports: [TenantModule],
   controllers: [ProductController],
   providers: [ProductService]
 })
